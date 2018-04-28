21d8e4822009509f32403f93aacab41a60b4ccea
MattBosek <bosekmx@dukes.jmu.edu>
2018-04-28 15:50:11 -0400

    Laptop Database action validation

--- LaptopGUI.java
+++ LaptopGUI.java
@@ -13,6 +13,7 @@ import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
@@ -294,17 +295,24 @@ public class LaptopGUI {
     {
         fingerprintReader = "no";
     }
+       
+       
      if (rdoCreate.isSelected()) 
      {
      insertItem();
      }
-     if (rdoUpdate.isSelected())
+     else if(rdoUpdate.isSelected())
      {
          updateItem();
      }
-     if (rdoDelete.isSelected())
+     else if (rdoDelete.isSelected())
      {
          deleteItem();
+     } else 
+     {
+    	 //Makes user select database action (create, update, delete)
+    	 Alert rdoAlert = new Alert(AlertType.ERROR, "Please select Create, Update, or delete.");
+	     rdoAlert.showAndWait();
      }
  });
     
